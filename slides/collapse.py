a732b442d0190aa459ad83d52da8791d84bc813d
Jeremy Roberts <jaroberts@ksu.edu>
2018-02-01 14:57:56 -0600

    Lec 3 things

---
+++ slides/collapse.py
@@ -0,0 +1,92 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import quad
+
+# load data
+E, sigma_ar, sigma_sr = np.loadtxt('u238_xs.txt', unpack=True)
+
+# Spectrum functions
+def narrow_resonance(E, sigma_ar, sigma_sr, sigma_d):
+    phi = 1/((sigma_ar+sigma_sr+sigma_d)*E)
+    return phi/phi[0]
+def wide_resonance(E, sigma_ar, sigma_sr, sigma_d):
+    phi = 1/((sigma_ar+sigma_d)*E)
+    return phi/phi[0]
+
+# group boundaries (from WIMS 69-group format.)
+group_boundaries = [27.7, 15.968, 9.877, 4.0] 
+
+# dilution cross sections
+inf_dilute = 100000000
+sigma_d = [inf_dilute, 10000000, 1000000, 100000, 10000, 1000, 100, 10, 1]
+
+# produce fluxes at each dilution
+phi_nr = {}
+phi_wr = {}
+for sd in sigma_d:
+    phi_nr[sd] = narrow_resonance(E, sigma_ar, sigma_sr, sd)
+    phi_wr[sd] = wide_resonance(E, sigma_ar, sigma_sr, sd)
+
+# produce cross sections
+sigma_g = {}
+for sd in sigma_d:
+    sigma_g[sd] = [0]*(len(group_boundaries)-1)
+    # define the integrands for the numerator and denominator of the
+    # group cross section using linear interpolation.
+    top_integrand = lambda x: np.interp(x, E, sigma_ar*phi_nr[sd])
+    bot_integrand = lambda x: np.interp(x, E, phi_nr[sd])
+    for g in range(len(sigma_g[sd])):
+        E_high = group_boundaries[g]
+        E_low = group_boundaries[g+1]
+        sigma_g[sd][g] = quad(top_integrand, E_low, E_high)[0] / \
+                         quad(bot_integrand, E_low, E_high)[0]
+
+# compute self-shielding factor
+self_shielding = []
+for sd in sigma_d:
+      self_shielding.append(sigma_g[sd][2] / sigma_g[inf_dilute][2])
+             
+
+plt.figure(1)
+plt.title('NR vs WR')
+plt.loglog(E, phi_nr[100], label='NR ($\sigma_d=100$ b)', color='b', ls='-')
+plt.loglog(E, phi_wr[100], label='WR ($\sigma_d=100$ b)', color='r', ls='-')
+plt.axis([1e0, 1e2, 1e-5, 1e-1])
+plt.legend()
+plt.xlabel('$E$ (eV)')
+plt.ylabel('$phi(E)$')
+
+plt.figure(2)
+plt.title('Impact of Dilution')
+plt.loglog(E, phi_nr[inf_dilute], label='NR ($\sigma_d=\infty$ b)', color='k', ls='-')
+plt.loglog(E, phi_nr[100], label='NR ($\sigma_d=100$ b)', color='b', ls='-')
+plt.loglog(E, phi_nr[10], label='NR ($\sigma_d=10$ b)', color='r', ls='-')
+plt.axis([1e0, 1e2, 1e-5, 1e-1])
+plt.legend()
+plt.xlabel('$E$ (eV)')
+plt.ylabel('$phi(E)$')
+
+
+plt.figure(3)
+plt.title('Group vs Continuous Cross Sections')
+plt.loglog(E, sigma_ar)
+for sd in sigma_d:
+    color = np.random.rand(3)
+    E_plt, sigma_plt = [], []
+    for g in range(3):
+        E_plt.append(group_boundaries[g])
+        E_plt.append(group_boundaries[g+1])
+        sigma_plt.append(sigma_g[sd][g])
+        sigma_plt.append(sigma_g[sd][g])        
+    plt.plot(E_plt, sigma_plt, c=color, 
+             label='$\sigma_d={}$'.format(sd if sd < 10000 else "\infty"))
+plt.axis([group_boundaries[-1], group_boundaries[0], 1e-1, 1e5])
+plt.legend()
+plt.xlabel('energy (eV)')
+plt.ylabel('cross section (b)')
+
+plt.figure(4)
+plt.title('Self-Shielding Factor (4-9.9 eV group)')
+plt.semilogx(sigma_d[1:], self_shielding[1:], '-o')
+
+plt.show()
--- slides/slbw.py
+++ slides/slbw.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 # potential cross section
 sigma_p = 11.2934
+# radius, from sigma_p = 4*pi*R^2
+R = np.sqrt(sigma_p*1e-24/4/np.pi)
+
 I = 0 # s wave
 E0, J, GN, GG, _, _ = np.loadtxt('u238.txt', unpack=True)
 G = GN+GG
@@ -15,12 +18,30 @@ sigma_n[:] = sigma_p
 for i in range(len(E0)):
     # statistical spin factor, where I=nuclear spint, J=total spin
     g = (2*J[i]+1)/(2*(2*I+1))
+
+    # reduced wavelength at resonance (in cm)
+    lambda_0 = 2.86e-9/np.sqrt(E0[i])/np.pi/2
     
     # total cross section at resonance energy (DH 2-36)
     sigma_0 = 2.608e6 * (A+1)**2/(A**2 * E0[i]) * (GN[i]/G[i]) * g
      
     # capture cross section (DH 2-35)
     y = (2/G[i])*(E - E0[i])
-    sigma_g += sigma_0 * (GN[i]/G[i]) * np.sqrt(E0[i]/E) * (1/(1+y**2))
+    sigma_g += sigma_0 * (GG[i]/G[i]) * np.sqrt(E0[i]/E) * (1/(1+y**2))
     
-plt.loglog(E, sigma_g)            
+    # scattering cross section (DH 2-3?)
+    sigma_n += sigma_0 * (GN[i]/G[i]) * np.sqrt(E0[i]/E) * (1/(1+y**2)) \
+             + sigma_0 * 2*R/lambda_0 * (y/(1+y**2)) 
+
+# add potential after other terms
+sigma_n += sigma_p
+
+plt.figure(1)
+plt.loglog(E, sigma_g, E, sigma_n)
+plt.legend(['capture', 'elastic'])
+plt.xlabel('energy (eV)')
+plt.ylabel('cross section (b)')            
+plt.show()
+
+# Save the data for collapsing demo
+np.savetxt('u238_xs.txt', np.transpose(np.array([E, sigma_g, sigma_n])))
